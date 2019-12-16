1196b6a470e5550b93133fd37a387561e1bc3b2c
Kelly Willard <32944774+kwillard95@users.noreply.github.com>
2019-12-15 22:55:42 -0800

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const mongoUri = 'mongodb://database/';
-
-const db = mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
-
-module.exports = db;
