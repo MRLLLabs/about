2c59f4c080d875631d86d19a57a57082bba6a9d2
Desmond Rogers <desmondrogers2@gmail.com>
2019-12-17 23:55:12 -0800

    Extend api to include routes for CRUD actions

--- server/index.js
+++ server/index.js
@@ -1,28 +1,73 @@
-const express = require('express')
-const app = express()
-const port = 3003;
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT;
 const path = require('path');
 const bodyParser = require('body-parser');
-const Company = require('../database/Company.js');
 const cors = require('cors');
+const Company = require('../database/Company.js');
 
 app.use('/', express.static(path.join(__dirname, '../dist')));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.post('/api/companies', (req, res) => {
+  const newCompany = new Company(req.body);
+  newCompany.save((err) => {
+    if (err) {
+      return res.status(400).send(err);
+    }
+    return res.status(201).send(newCompany);
+  });
+});
 
-app.get('/about/getData/:id', function(req, res) {
-    var id = req.params.id;
-   Company.findOne({id: id}, (err, result) => {
-       if (err) {
-           console.log(err)
-       } else {
-           console.log(result)
-           res.send(result);
-       }
-   })
-})
+app.get('/api/companies', (res) => {
+  Company.find((err, companies) => {
+    if (err) {
+      return res.status(404).send(err);
+    }
+    return res.send(companies);
+  });
+});
 
+app.get('/api/companies/:id', (req, res) => {
+  const { id } = req.params;
+  Company.findOne({ id }, (err, result) => {
+    if (err) {
+      res.status(404).send(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.put('/api/companies/:id', (req, res) => {
+  Company.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }, // return the updated version
+    (err, company) => {
+      if (err) {
+        return res.status(404).send(err);
+      }
+      return res.send(company);
+    },
+  );
+});
+
+app.delete('/api/companies/:id', (req, res) => {
+  Company.findByIdAndRemove(req.params.id, (err, company) => {
+    if (err) {
+      return res.status(400).send(err);
+    }
+    const response = {
+      message: 'Company successfully deleted',
+      // eslint-disable-next-line no-underscore-dangle
+      id: company._id,
+    };
+    return res.status(200).send(response);
+  });
+});
+
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
